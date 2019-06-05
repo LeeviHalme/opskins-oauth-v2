a855cfc0da126ebce4d499868fa450838acd2bf0
BuildTools <unconfigured@null.spigotmc.org>
2019-06-06 01:52:57 +0300

    First commit! :rocket:

---
+++ README.md
@@ -0,0 +1,111 @@
+# opskins-oauth-v2
+
+OPSkins-oAuth-v2 is a simple wrapper package around OPSkins oAuth API. Inspired by almatrass's package called `opskins-oauth`. That package is currently deprecated but the new one supports only PassportJS. This is a rebuilt authentication wrapper package, which supports promises.
+
+## Usage
+
+Install the package by typing `npm i opskins-oauth-v2` in your project folder.
+
+### Setup
+
+```javascript
+const OpskinsAuth = require("opskins-oauth-v2");
+
+const opskins = new OpskinsAuth({
+  name: "testing", // Site name displayed to users on logon
+  returnUrl: "http://localhost:5000/auth/opskins/authenticate", // Your return route
+  apiKey: "2087fcb59f2be98c8a5bbfe245669d", // OPSkins API key
+  scopes: "identity_basic trades", // Scopes you want to access
+  mobile: true, // Removes login navbar if true
+  permanent: true // Maintains permanent access and gives refresh token if true
+});
+```
+
+### Routes
+
+```javascript
+app.get("/auth/opskins", async (req, res) => {
+  const redirectUrl = await opskins.getRedirectUrl();
+  return res.redirect(redirectUrl);
+});
+
+app.get("/auth/opskins/authenticate", async (req, res) => {
+  try {
+    const user = await opskins.authenticate(req);
+
+    //...do something with the data
+  } catch (error) {
+    console.error(error);
+  }
+});
+```
+
+## Methods
+
+### getRedirectUrl
+
+Gets the redirect URL to OPSkins.
+
+#### Parameters
+
+None
+
+#### Returns
+
+- Promise (String)
+
+#### Example
+
+```javascript
+opskins.getRedirectUrl().then(url => {
+  //...do something with the url
+});
+```
+
+### authenticate
+
+Authenticates the user with oAuth.
+
+#### Parameters
+
+- request (ExpressJsRequest, Object)
+
+#### Returns
+
+- Promise (UserObject)
+
+#### Example
+
+```javascript
+opskins.authenticate(req).then(user => {
+  //...do something with the user
+});
+```
+
+## Objects
+
+### UserObject
+
+Object which holds all the authenticated user's data. The `refresh_token` key is additional so it's emitted only when `permanent` is set to `true`.
+
+#### Example
+
+```javascript
+{
+  id: 1234567,
+  id64: "12345678912345678",
+  username: "Example Username",
+  avatar: "...",
+  preferred_currency: 123,
+  preferred_lang: "eng",
+  balance: 0,
+  credits: 0,
+  cryptoBalances: 0,
+  access_token: "...",
+  refresh_token: "...",
+}
+```
+
+## License
+
+MIT <3
---
+++ index.js
@@ -0,0 +1,187 @@
+// Require Dependencies
+const axios = require("axios");
+const crypto = require("crypto");
+
+// Main Class
+class OpskinsAuth {
+  constructor({ name, returnUrl, apiKey, scopes, mobile, permanent }) {
+    if (!name || !returnUrl || !apiKey)
+      throw new Error(
+        "Missing name, returnURL or apiKey parameter(s). These are required."
+      );
+
+    this.name = name;
+    this.returnUrl = returnUrl;
+    this.plainTextApiKey = apiKey;
+    this.scopes = scopes || "identity";
+    this.mobile = mobile || false;
+    this.permanent = permanent || false;
+    this.states = [];
+
+    // Setup oAuth Client
+    this.setupClient();
+  }
+
+  async setupClient() {
+    // Get api key that is used for requests
+    this.apiKey = Buffer.from(this.plainTextApiKey + ":", "ascii").toString(
+      "base64"
+    );
+
+    try {
+      let config = {
+        method: "GET",
+        url: "https://api.opskins.com/IOAuth/GetOwnedClientList/v1/",
+        headers: {
+          Authorization: `Basic ${this.apiKey}`,
+          "Content-Type": "application/json; charset=utf-8"
+        }
+      };
+
+      let {
+        data: { response }
+      } = await axios(config);
+
+      // Remove existing clients
+      for (let index = 0; index < response.clients.length; index++) {
+        const client = response.clients[index];
+
+        if (client.name === this.name) {
+          const config = {
+            method: "POST",
+            url: "https://api.opskins.com/IOAuth/DeleteClient/v1/",
+            headers: {
+              Authorization: `Basic ${this.apiKey}`,
+              "Content-Type": "application/x-www-form-urlencoded"
+            },
+            data: `client_id=${client.client_id}`
+          };
+
+          const { data } = await axios(config);
+          if (data.status !== 1)
+            throw new Error("Error with the OPSkins API: " + data.message);
+        }
+      }
+
+      // Create a new client
+      config = {
+        method: "POST",
+        url: "https://api.opskins.com/IOAuth/CreateClient/v1/",
+        headers: {
+          Authorization: `Basic ${this.apiKey}`,
+          "Content-Type": "application/json; charset=utf-8"
+        },
+        data: {
+          name: this.name,
+          redirect_uri: this.returnUrl
+        }
+      };
+
+      let { data: apiResponse } = await axios(config);
+      let {
+        response: { client }
+      } = apiResponse;
+
+      if (apiResponse.status !== 1)
+        throw new Error("Error with the OPSkins API: " + apiResponse.message);
+
+      this.clientId = client.client_id;
+      this.clientSecret = apiResponse.response.secret;
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        throw new Error(
+          "API Key is invalid. Remember that API keys are CaSe SeNsItIvE."
+        );
+      } else {
+        console.error(error);
+      }
+    }
+  }
+
+  // Get redirect url for OPSkins
+  async getRedirectUrl() {
+    return new Promise(async resolve => {
+      const random = crypto.randomBytes(4).toString("hex");
+      this.states = [...this.states, random];
+
+      const redirectUrl = `https://oauth.opskins.com/v1/authorize?state=${random}&client_id=${
+        this.clientId
+      }&response_type=code&scope=${this.scopes}${
+        this.mobile ? "&mobile=1" : ""
+      }${this.permanent ? "&duration=permanent" : ""}`;
+      resolve(redirectUrl);
+    });
+  }
+
+  // Authenticate user
+  async authenticate(req) {
+    return new Promise(async (resolve, reject) => {
+      if (req.query.error) reject("User denied access.");
+      if (!req.query.code) reject("No authorization code provided.");
+
+      // Check if authentication originated from this server
+      let originated = false;
+      this.states.map(
+        state => state === req.query.state && (originated = true)
+      );
+
+      if (!originated)
+        reject("Authentication did not originate from this server.");
+
+      try {
+        const authKey = Buffer.from(
+          this.clientId + ":" + this.clientSecret
+        ).toString("base64");
+
+        // Get user access token
+        let config = {
+          method: "POST",
+          url: "https://oauth.opskins.com/v1/access_token",
+          headers: {
+            Authorization: `Basic ${authKey}`,
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+          data: `grant_type=authorization_code&code=${req.query.code}`
+        };
+
+        let response = await axios(config);
+        const accessToken = response.data.access_token;
+        const refreshToken = response.data.refresh_token;
+
+        // Get user profile
+        config = {
+          method: "GET",
+          url: "https://api.opskins.com/IUser/GetProfile/v1/",
+          headers: {
+            Authorization: `Bearer ${accessToken}`
+          }
+        };
+
+        response = await axios(config);
+
+        if (response.data.status !== 1)
+          throw new Error(
+            "Error with the OPSkins API: " + response.data.message
+          );
+
+        const user = {
+          ...response.data.response,
+          balance: response.data.balance,
+          credits: response.data.credits,
+          cryptoBalances: response.data.cryptoBalances,
+          accessToken,
+          refreshToken
+        };
+
+        resolve(user);
+      } catch (error) {
+        if (error.response && error.response.status === 401) {
+          console.log(error.response);
+        }
+      }
+    });
+  }
+}
+
+// Export class
+module.exports = OpskinsAuth;
